259ad08000352c92595d5e2100a534b9942aa8e7
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2023-04-16 20:38:24 +0700

    Add icon to main page

--- src/containers/Dashboard/Main/Main.js
+++ src/containers/Dashboard/Main/Main.js
@@ -3,6 +3,14 @@ import { useRouter } from "next/router";
 import { ROUTES } from "src/configs";
 import useAction from "./hooks/useAction";
 import clsx from "clsx";
+import {
+  QrCodeIcon,
+  LinkIcon,
+  ShareIcon,
+  ChatBubbleLeftEllipsisIcon,
+  PencilSquareIcon,
+  DeviceTabletIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Main() {
   const router = useRouter();
@@ -10,34 +18,70 @@ export default function Main() {
 
   const MENU = [
     {
-      label: "Buka Ucapan",
+      label: (
+        <>
+          Buka <br />
+          Ucapan
+        </>
+      ),
       onClick: () => window.open(cardData.link, "_blank"),
       className: "bg-gradient-to-br from-indigo-800 to-light-blue-500",
+      icon: DeviceTabletIcon,
     },
     {
-      label: "Salin Link Ucapan",
-      onClick: () => copyLink(),
-      className: "bg-gradient-to-br from-indigo-800 to-light-blue-500",
-    },
-    {
-      label: "Generate QR Code",
-      onClick: () => router.push(ROUTES.GENERATE_QR()),
-      className: "bg-gradient-to-br from-indigo-800 to-light-blue-500",
-    },
-    {
-      label: "Share ke Whatsapp",
-      onClick: () => router.push(ROUTES.SHARE()),
-      className: "bg-gradient-to-br from-green-800 to-light-blue-500",
-    },
-    {
-      label: "Lihat Komentar",
-      onClick: () => router.push(ROUTES.COMMENTS()),
-      className: "bg-gradient-to-br from-indigo-800 to-light-blue-500",
-    },
-    {
-      label: "Ubah Ucapan",
+      label: (
+        <>
+          Ubah <br />
+          Ucapan
+        </>
+      ),
       onClick: () => router.push(ROUTES.THEME()),
       className: "bg-gradient-to-br from-indigo-800 to-light-blue-500",
+      icon: PencilSquareIcon,
+    },
+    {
+      label: (
+        <>
+          Salin Link <br />
+          Ucapan
+        </>
+      ),
+      onClick: () => copyLink(),
+      className: "bg-gradient-to-br from-indigo-800 to-light-blue-500",
+      icon: LinkIcon,
+    },
+    {
+      label: (
+        <>
+          Share ke <br />
+          WhatsApp
+        </>
+      ),
+      onClick: () => router.push(ROUTES.SHARE()),
+      className: "bg-gradient-to-br from-green-800 to-light-blue-500",
+      icon: ShareIcon,
+    },
+    {
+      label: (
+        <>
+          Generate <br />
+          QR Code
+        </>
+      ),
+      onClick: () => router.push(ROUTES.GENERATE_QR()),
+      className: "bg-gradient-to-br from-indigo-800 to-light-blue-500",
+      icon: QrCodeIcon,
+    },
+    {
+      label: (
+        <>
+          Lihat <br />
+          Komentar
+        </>
+      ),
+      onClick: () => router.push(ROUTES.COMMENTS()),
+      className: "bg-gradient-to-br from-indigo-800 to-light-blue-500",
+      icon: ChatBubbleLeftEllipsisIcon,
     },
   ];
 
@@ -49,15 +93,16 @@ export default function Main() {
         </div>
       </div>
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
-        {MENU.map(({ label, className, onClick }, index) => (
+        {MENU.map(({ label, className, onClick, icon: Icon }, index) => (
           <div
             className={clsx(
-              "cursor-pointer rounded-lg p-4 h-36 hover:outline outline-2 outline-offset-2 outline-light-blue-500",
+              "cursor-pointer rounded-lg p-4 h-36 hover:outline outline-2 outline-offset-2 outline-light-blue-500 flex flex-col justify-between",
               className
             )}
             key={index}
             onClick={() => !loading && onClick()}
           >
+            <Icon className="h-10 w-10" />
             <h2 className="text-xl uppercase">
               {loading ? "Memuat..." : label}
             </h2>
