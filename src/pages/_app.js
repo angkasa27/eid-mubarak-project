652f605c424afb287ebcf643bca8a6909b5bd55a
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2023-04-16 23:58:25 +0700

    Add google console analytic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -24,14 +24,30 @@ export default function App({ Component, pageProps }) {
   return (
     <Fragment>
       <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-V5N6QP5EJP"
+        strategy="afterInteractive"
+      />
+      <Script
         dangerouslySetInnerHTML={{
           __html: `
-        (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "gmbat8atg7");
-      `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V5N6QP5EJP');
+          `,
+        }}
+        id="google-analytics"
+        strategy="afterInteractive"
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "gmbat8atg7");
+          `,
         }}
         id="microsoft-clarity"
         strategy="afterInteractive"
